f1fb9abf40582c67a014bbc3301f710a9bab7130
ThomasTejGor <thomastejgor@gmail.com>
2022-04-25 08:52:19 +0200

    heroku act

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -34,12 +34,12 @@
     </p>
     <ul>
         <li><a href="https://sos2122-13.herokuapp.com/api/v1/immigrants-stats/docs">[https://sos2122-13.herokuapp.com/api/v1/immigrants-stats/docs]</a>
-            <a href="https://sos2122-13.herokuapp.com/#/immigrants-stats">[https://sos2122-13.herokuapp.com/#/immigrants-stats]</a>
+            <a href="https://sos2122-13.herokuapp.com/#/immigrants-stats">[https://sos2122-13.herokuapp.com/#/immigrants]</a>
             (developed by <a href="https://github.com/ThomasTejGor">
             [Thomas Tejeda Gordon]
         </a>)</li>
         <li><a href="https://sos2122-13.herokuapp.com/api/v1/emigrants-stats/docs">[https://sos2122-13.herokuapp.com/api/v1/emigrants-stats/docs]</a>
-            <a href="https://sos2122-13.herokuapp.com/#/emigrants-stats">[https://sos2122-13.herokuapp.com/#/emigrants-stats]</a>
+            <a href="https://sos2122-13.herokuapp.com/#/emigrants-stats">[https://sos2122-13.herokuapp.com/#/emigrants]</a>
             (developed by <a href="https://github.com/celiasg21">
             [Celia Sánchez Gaitán]
         </a>)</li>
