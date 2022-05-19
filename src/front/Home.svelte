fbb4e9dcb6865899b470418fadf979efd1cac2c5
celsangai <celiasanchezetsii@gmail.com>
2022-05-19 20:38:19 +0200

    actualización home

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -30,15 +30,15 @@
 
             <h2>Repository: </h2>
             <br>
-            <a href="https://github.com/gti-sos/SOS2122-13">
+            <!--<a href="https://github.com/gti-sos/SOS2122-13">
                 [gti-sos/SOS2122-13]
-            </a>
+            </a>-->
+            <button onclick="window.location.href='https://github.com/gti-sos/SOS2122-13'">Repositorio</button>
             <hr>
 
             <h2>Heroku: </h2>
             <br>
-            <a href="http://sos2122-13.herokuapp.com">[http://sos2122-13.herokuapp.com]</a>
-
+            <button onclick="window.location.href='http://sos2122-13.herokuapp.com'">Heroku</button>
             <hr>
 
             
@@ -56,60 +56,65 @@
             <p>(developed by <a href="https://github.com/celiasg21">
                 [Celia Sánchez Gaitán])</a>
             </p>
+        
             <hr>
-
+            
             <h2>Documentación: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/api/v1/emigrants/docs">[https://sos2122-13.herokuapp.com/api/v2/emigrants/docs]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/api/v2/emigrants/docs'">Documentación</button>
             <hr>
 
             <h2>Front End: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/#/emigrants">[https://sos2122-13.herokuapp.com/#/emigrants]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/#/emigrants'">Front End</button>
             <hr>
 
             <h2>API: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/api/v2/emigrants">[https://sos2122-13.herokuapp.com/api/v2/emigrants]</a>
-            <a href="https://sos2122-13.herokuapp.com/api/v1/emigrants">[https://sos2122-13.herokuapp.com/api/v1/emigrants]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/api/v2/emigrants'">API v2</button>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/api/v1/emigrants'">API v1</button>
             <hr>
 
             <h2>Gráfica: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/#/emigrants-graph">[GRÁFICA EMIGRANTES]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/#/emigrants-graph'">GRÁFICA EMIGRANTES</button>
             <hr>
 
         </div>
 
-        <div class= "immigrants" ALIGN = "center">
-            <h1>Immigrantes</h1>
+        <div class= "emigrants" ALIGN = "center">
+
+            <h1>Emigrantes</h1>
             <p>(developed by <a href="https://github.com/ThomasTejGor">
                 [Thomas Tejeda Gordon])</a>
             </p>
+        
             <hr>
-
+            
             <h2>Documentación: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/api/v1/immigrants/docs">[https://sos2122-13.herokuapp.com/api/v2/emigrants/docs]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/api/v1/immigrants/docs'">Documentación</button>
             <hr>
 
             <h2>Front End: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/#/immigrants-stats">[https://sos2122-13.herokuapp.com/#/emigrants]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/#/immigrants-stats'">Front End</button>
             <hr>
 
             <h2>API: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/api/v2/immigrants">[https://sos2122-13.herokuapp.com/api/v2/emigrants]</a>
-            <a href="https://sos2122-13.herokuapp.com/api/v1/immigrants">[https://sos2122-13.herokuapp.com/api/v1/emigrants]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/api/v2/immigrants'">API v2</button>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/api/v1/immigrants'">API v1</button>
             <hr>
 
             <h2>Gráfica: </h2>
             <br>
-            <a href="https://sos2122-13.herokuapp.com/#/Immigrants-graph">[GRÁFICA INMIGRANTES]</a>
+            <button onclick="window.location.href='https://sos2122-13.herokuapp.com/#/Immigrants-graph'">GRÁFICA IMMIGRANTS</button>
             <hr>
 
         </div>
+
+        
 
     </div>
 
